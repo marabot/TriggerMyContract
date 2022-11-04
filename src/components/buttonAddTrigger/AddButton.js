fe33dabbbaad6948f271e02fee2b58e67f4337f7
Charcosset Antoine <antoine.charcosset@sfr.fr>
2022-11-04 06:58:52 +0100

    interface paramaters in create triggers popup

--- src/components/buttonAddTrigger/AddButton.js
+++ src/components/buttonAddTrigger/AddButton.js
@@ -1,6 +1,5 @@
 import React, { useEffect,useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import {Modal, Button, Form} from 'react-bootstrap';
 import {addToDB} from '../../utils/firebase.js';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
@@ -16,22 +15,36 @@ function AddButton({userAddress, chainId}){
     const [UserAddr, setUserAddr] = useState('');
     const [IsIntervalEvery, SetIsIntervalEvery] = useState();
     const [startDate, setStartDate] = useState(new Date());
-    const [show, setShow] = useState(false); 
+    const [Show, setShow] = useState(false); 
     const [Days, setDays] = useState(0);
     const [Hours, setHours] = useState(0);
     const [Minutes, setMinutes] = useState(0);
-    
+
+    const [IsLoaded, setIsLoaded] = useState(false);
+
+    const [ParamType1, setParamType1] = useState('none');
+    const [ParamValue1, setParaValue1]= useState('');
+    const [ParamType2, setParamType2] = useState('none');
+    const [ParamValue2, setParaValue2]= useState('');
+    const [ParamType3, setParamType3] = useState('none');
+    const [ParamValue3, setParaValue3]= useState('');
     
     const handleClose = () => setShow(false);        
     
     const handleCloseAndSave = () => {    
         let lastTick;
-        if (IsIntervalEvery)
-        {
-            lastTick=0;
-        } else {           
-            lastTick = Timestamp.fromDate(startDate).seconds - Timing;          
-        }
+        lastTick = IsIntervalEvery?0:Timestamp.fromDate(startDate).seconds - Timing;       
+
+        const params='';
+        if (ParamType1==='none'){
+            params=';;';
+        }else if (ParamType2==='none'){
+            params = ParamValue1 + ';;';
+        }else if (ParamType3==='none'){
+            params = ParamValue1+ ';' + ParamValue2 + ';';
+        }else{
+            params = ParamValue1+ ';' + ParamValue2 + ';'+ ParamValue3;
+        }        
        
        addToDB(Label, userAddress, chainId, ToContract, FunctionToCall, Timing, lastTick );      
        setToContract('');
@@ -40,8 +53,10 @@ function AddButton({userAddress, chainId}){
        setShow(false);
     };
 
-    const handleShow = () => {          
-        setShow(true);          
+    const handleShow = () => {    
+        
+        setShow(true);     
+       
     };
 
     const datepicker= () => {
@@ -70,6 +85,29 @@ function AddButton({userAddress, chainId}){
         justifyContent:"space-around",
      }
 
+     const parameter={
+        display:"flex",    
+        width:"90%",
+        justifyContent:"space-around",
+     }
+
+     const parameterValue={       
+        width:"290px",
+        
+     }
+
+     function onLoad() {
+        console.log("onload");
+        const divParam2= document.querySelector("#param2");
+        const divParam3=  document.querySelector("#param3");
+        divParam2.style.display='none';
+        divParam3.style.display = 'none';
+      }
+
+    useEffect(()=>{      
+        setParamType1('none');
+    },[]);
+
     useEffect(()=>{        
          
        
@@ -84,6 +122,30 @@ function AddButton({userAddress, chainId}){
         const radioStartNow = document.querySelector("#startNow");
         const radioStartat = document.querySelector("#startAt");
         const datePicker = document.querySelector("#datepicker");
+        const selectType1 = document.querySelector("#selectType1");
+        const selectType2 = document.querySelector("#selectType2");
+        const selectType3 = document.querySelector("#selectType3");
+        const divParam2= document.querySelector("#param2");
+        const divParam3=  document.querySelector("#param3");
+       
+        console.log(selectType1);
+        console.log(selectType2);
+        if (selectType1 ){
+            console.log(selectType1.value==="none");
+          
+            if (ParamType1==='none'){
+                divParam2.style.display='none';
+                divParam3.style.display = 'none';
+            }
+            else
+            {
+                divParam2.style.display='flex';
+                divParam2.style.flexDirection="column"
+                divParam3.style.display = ParamType2==='none'? "none":"flex"; 
+                divParam3.style.flexDirection="column"; 
+            }    
+        }
+
 
         if (radioStartNow && radioStartat)
         {
@@ -98,7 +160,7 @@ function AddButton({userAddress, chainId}){
             {
                radioStartNow.checked = 0;
                radioStartat.checked = 1;
-               datePicker.style.display = "block";
+               datePicker.style.display = "flex";
             }
         }
         
@@ -106,14 +168,16 @@ function AddButton({userAddress, chainId}){
         const timing= parseInt(Days)*3600*24 +  parseInt(Hours)*3600 + parseInt(Minutes)*60;
         setTiming(timing);
        
-      },[userAddress, chainId, Days, Hours, Minutes, IsIntervalEvery]);
+      },[userAddress, chainId, Days, Hours, Minutes, IsIntervalEvery, ParamType1, ParamType2, ParamType3, Show]);
     
+
+
     return(            
        
         <div>
             <Button id="buttonAdd" variant="primary" onClick={handleShow}> Create new Trigger </Button> 
            
-             <Modal show={show} style={{opacity:1}} animation={false} onHide={handleClose}>    
+             <Modal show={Show} style={{opacity:1}} animation={false} onHide={handleClose}>    
             
                     <Modal.Header closeButton>
                             <Modal.Title>Create new Trigger</Modal.Title>
@@ -121,22 +185,79 @@ function AddButton({userAddress, chainId}){
                     <Modal.Body>
                         <form>    
                         <label>Name</label><br/>
-                        <input value={Label} type="text" name="to" onChange={e=>setLabel(e.target.value)}/><br/>
+                        <input value={Label} type="text" name="to" onChange={e=>setLabel(e.target.value)} style ={parameterValue}/><br/>
                         <label>to</label><br/>
-                        <input value={ToContract} type="text" name="to" onChange={e=>setToContract(e.target.value)}/><br/>
+                        <input value={ToContract} type="text" name="to" onChange={e=>setToContract(e.target.value)} style ={parameterValue}/><br/>
                         <label>function to call</label><br/>
-                        <input value={FunctionToCall} type="text" name="function" onChange={e=>setFunctionToCall(e.target.value)}/><br/>
-                        <label>Interval</label><br/>    
-                        <div>        
+                        <input value={FunctionToCall} type="text" name="function" onChange={e=>setFunctionToCall(e.target.value)} style ={parameterValue}/><br/><br/>
+                        
+                        <div>Parameter 1
+                            <div style ={parameter}>
+                                <div>
+                                    <Form.Select aria-label="Default select example" size ="sm" id="selectType1" onChange={e=>setParamType1(e.target.value)}>
+                                        <option value="none">None</option>
+                                        <option value="uint">uint</option>
+                                        <option value="string">string</option>
+                                        <option value="bool">boolean</option>
+                                    </Form.Select>
+                                
+                                </div>
+                                <div >
+                                <input value={Label} type="text" name="to" onChange={e=>setParaValue1(e.target.value)} style ={parameterValue}/><br/>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div id="param2">
+                            <div>Parameter 2</div>
+                            <div style ={parameter} >
+                            
+                                <div>
+                                    <Form.Select aria-label="Default select example" size ="sm" id="selectType2" onChange={e=>setParamType2(e.target.value)}>
+                                        <option value="none">None</option>
+                                        <option value="uint">uint</option>
+                                        <option value="string">string</option>
+                                        <option value="bool">boolean</option>
+                                    </Form.Select>
+                                
+                                </div>
+                                <div >
+                                <input value={Label} type="text" name="to" onChange={e=>setParaValue2(e.target.value)} style ={parameterValue}/><br/>
+                                </div>
+                                
+                            </div> 
+                        </div>
+                        <div id= "param3">Parameter 3
+                            <div style ={parameter} >                        
+                                <div>
+                                    <Form.Select aria-label="Default select example" size ="sm" id="selectType2" onLoad={()=>onLoad()} onChange={e=>setParamType3(e.target.value)}>
+                                        <option value="none">None</option>
+                                        <option value="uint">uint</option>
+                                        <option value="string">string</option>
+                                        <option value="bool">boolean</option>
+                                    </Form.Select>
+                                
+                                </div>
+                                <div >
+                                <input value={Label} type="text" name="to" onChange={e=>setParaValue3(e.target.value)} style ={parameterValue}/><br/>
+                                </div>
+                                
+                            </div>
+                        </div><br/>
+                        <label>Interval</label><br/> 
+                        
+                        <div id="start">
+                            <div><input name="now" type="radio"  id="startNow" onChange={()=>SetIsIntervalEvery(true)}/> Now 
+                            <div>        
                             <div  id ="interval" style={interval}>
                                 <div > Days : <input style={selectInterval} type="text" value={Days} name="d" id="days" onChange={e=>setDays(e.target.value)}/> </div> +
                                 <div > Hours : <input style={selectInterval} type="text" value={Hours} name="h" id="hours" onChange={e=>setHours(e.target.value)}/> </div> +
                                 <div > Minutes : <input style={selectInterval} type="text" value={Minutes} name="m" id="minutes" onChange={e=>setMinutes(e.target.value)}/> </div>
                             </div>
                         </div>     
-                        <div id="start">
-                            <div><input name="now" type="radio" label="now" value ="now" id="startNow" onChange={()=>SetIsIntervalEvery(true)}/> Now </div>    
-                            <div><input name="at" type="radio" label="at" value ="now" id="startAt" onChange={()=>SetIsIntervalEvery(false)}/> At a time</div>
+                            
+                            </div>    
+                            <div><input name="at" type="radio"  id="startAt" onChange={()=>SetIsIntervalEvery(false)}/> At a time</div>
                             <div id ="datepicker">{datepicker()}  </div>
                         </div>                   
                         </form>
